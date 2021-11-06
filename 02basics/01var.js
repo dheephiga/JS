847d2f8ac362eff34198d254e2a9ba110c026695
Dheephiga <64630313+dheane@users.noreply.github.com>
2021-11-06 14:39:40 +0530

    Add files via upload

---
+++ 02basics/01var.js
@@ -0,0 +1,16 @@
+var name = "Alligator";
+
+// comments
+
+var isloggedin = false;
+var islogged = "false";
+
+var count = 38;
+
+console.log(count)
+console.log("count")
+
+var payment;
+payment = "credit";
+console.log(payment);
+
